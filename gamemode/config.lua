0cf1d87c2ffbf2073e39745b5013a3912773dcf8
GraphicKet <ketchupgaming@outlook.com>
2014-10-08 20:39:51 +0200

    Update config.lua

--- gamemode/config.lua
+++ gamemode/config.lua
@@ -5,18 +5,3 @@ ftimer.Americans = 0    -- start point
 ftimer.FragLimit = 150 -- first team to hit the limit wins.
 ftimer.givenpoint = 15 -- how many pointshop points should the killer get.
 blackout_time = 15     -- time till next round starts
-
-rebelmodels = {
-	"models/player/combine_soldier.mdl",  -- Yes I know there is only one model here atm.
-}
-americanmodels = {
-	"models/player/Group01/Male_01.mdl",
-	"models/player/Group01/Male_04.mdl",
-	"models/player/Group01/Male_07.mdl",
-	"models/player/Group01/Male_09.mdl",
-	"models/player/Group01/Female_01.mdl",
-	"models/player/Group01/Female_02.mdl",
-	"models/player/Group01/Female_03.mdl",
-	"models/player/Group01/Female_04.mdl",
-}
-
