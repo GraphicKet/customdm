5145e41521d92a1b30522a7d50e818bdb786b30f
Thalexs <thalesfilho1@hotmail.com>
2014-10-08 16:20:54 -0300

    Update config.lua

--- gamemode/config.lua
+++ gamemode/config.lua
@@ -1,8 +1,17 @@
 ftimer = {}
 
-ftimer.Rebels = 0  -    -- start frags
-ftimer.Americans = 0    -- start frags
+ftimer.Rebels = 0  -    -- start point
+ftimer.Americans = 0    -- start point
 ftimer.FragLimit = 150 -- first team to hit the limit wins.
-ftimer.givenpoint = 15 -- how many pointshop points the killer gets
-ftimer.winnerreward = 100 -- how many point the winning team players gets
+ftimer.givenpoint = 15 -- how many pointshop points should the killer get.
 blackout_time = 15     -- time till next round starts
+
+rebelmodels = {
+	"models/cod players/opfor3.mdl",  -- Yes I know there is only one model here atm.
+}
+americanmodels = {
+
+	"models/codmw2/t_codmw2.mdl",
+
+}
+
