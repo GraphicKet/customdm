a98b4655a390f36e81ee31c5ab8f33767d3c2c5b
GraphicKet <ketchupgaming@outlook.com>
2014-10-08 20:48:07 +0200

    Update config.lua

--- gamemode/config.lua
+++ gamemode/config.lua
@@ -1,7 +1,8 @@
 ftimer = {}
 
-ftimer.Rebels = 0  -    -- start point
-ftimer.Americans = 0    -- start point
+ftimer.Rebels = 0  -    -- start frags
+ftimer.Americans = 0    -- start frags
 ftimer.FragLimit = 150 -- first team to hit the limit wins.
-ftimer.givenpoint = 15 -- how many pointshop points should the killer get.
+ftimer.givenpoint = 15 -- how many pointshop points the killer gets
+ftimer.winnerreward = 100 -- how many point the winning team players gets
 blackout_time = 15     -- time till next round starts
