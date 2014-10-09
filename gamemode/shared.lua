3c7139354e1fb52e9fb771bc139c28d212693e39
GraphicKet <ketchupgaming@outlook.com>
2014-10-09 20:48:27 +0200

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -3,7 +3,7 @@ team.SetUp(TEAM_NONE, "None", Color(255, 0, 0, 255))
 team.SetUp(TEAM_AMERICAN, "American", Color(0, 255, 0, 255))
 team.SetUp(TEAM_REBEL, "Rebel", Color(255, 0, 0, 255))
 
-for k, v in pairs(file.Find("player_clases/*.lua") do 
+for k, v in pairs(file.Find("player_clases/*.lua")) do 
 include("player_classes/" .. v) 
 end
 
